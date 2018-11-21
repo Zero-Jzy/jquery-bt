64fee45332b7adc0be12d0e20887ac2e035d360e
phu <dekujzy@gmail.com>
2018-11-21 23:57:26 +0700

    ...

--- js/login.js
+++ js/login.js
@@ -1,39 +1,54 @@
-var LOGIN_API = 'https://2-dot-backup-server-002.appspot.com/_api/v2/members/authentication';
+var validater = $('#login-form').validate({
+    rules: {
+        email: {
+            required: true,
+            email: true
+        },
+        password: {
+            required: true,
+            minlength: 2,
+            maxlength: 15
+        }
+    },
+    messages: {
+        email: {
+            required: 'Vui lòng email của bạn.',
+            email: 'Vui lòng nhập email đúng định dạng'
+        },
+        password: {
+            required: 'Vui lòng nhập password.',
+            minlength: 'Password quá ngắn, vui lòng nhập ít nhất {0} ký tự',
+            maxlength: 'Password quá dài, vui lòng nhập nhiều nhất {0} ký tự',
+        }
+    },
+    submitHandler: function (form, event) {
+        event.preventDefault();
+        var senderObject = {
+            password: $(form['password']).val(),
+            email: $(form['email']).val(),
+        };
+        $.ajax({
+            url: LOGIN_API,
+            type: 'POST',
+            contentType: 'application/json; charset=utf-8',
+            data: JSON.stringify(senderObject),
+            success: function (data) {
+                alert(data.token);
+               localStorage.setItem('my-token',data.token)
+            },
+            error: function (jqXHR) {
+                alert(data.token);
+                if (Object.keys(jqXHR.responseJSON.error).length > 0) {
 
-document.addEventListener('DOMContentLoaded', function () {
-    var loginForm = document.forms['login-form'];
-    if (loginForm == null || loginForm['btn-submit'] == null) {
-        alert('Vui lòng thử lại!');
-        return;
-    }
-    loginForm['btn-submit'].onclick = function () {
-        var pwdPassword = loginForm['password'];
-        var txtEmail = loginForm['email'];
-        if (pwdPassword == null || txtEmail == null) {
-            alert('Vui lòng thử lại!');
-            return;
-        }
-        var jsMemberLogin = {
-            password: pwdPassword.value,
-            email: txtEmail.value,
-        }
-        doLogin(jsMemberLogin);
+                    $('#summary')
+                        .text(`please fix ${Object.keys(jqXHR.responseJSON.error).length} below!`);
+                    validater.showErrors(jqXHR.responseJSON.error);
+                    console.log(jqXHR.responseJSON.error);
+                    console.log(jqXHR)
+                }
+
+            }
+        });
+        return false;
     }
 });
-
-function doLogin(jsMemberLogin) {
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 201) {
-            var tokenObject = JSON.parse(this.responseText);
-            alert(`Đăng nhập thành công. Token là ${tokenObject.token}`);
-            localStorage.setItem('token-key', tokenObject.token);
-        }
-    }
-    xhr.open('POST', LOGIN_API, true);
-    xhr.setRequestHeader("Content-type", "application/json");
-    xhr.send(JSON.stringify(jsMemberLogin));
-}
-
-
-
--- js/save-song.js
+++ js/save-song.js
@@ -1,53 +1,156 @@
-var CREATE_SONG_API = 'https://2-dot-backup-server-002.appspot.com/_api/v2/songs';
+// document.addEventListener('DOMContentLoaded', function () {
+//     var songForm = document.forms['song-form'];
+//     if (songForm == null || songForm['btn-submit'] == null) {
+//         alert('Vui lòng thử lại!');
+//         return;
+//     }
+//     songForm['btn-submit'].onclick = function () {
+//         var txtName = songForm['name'];
+//         var txtDescription = songForm['description'];
+//         var txtSinger = songForm['singer'];
+//         var txtAuthor = songForm['author'];
+//         var txtThumbnail = songForm['thumbnail'];
+//         var txtLink = songForm['link'];
+//         if (txtName == null
+//             || txtDescription == null
+//             || txtSinger == null
+//             || txtAuthor == null
+//             || txtThumbnail == null
+//             || txtLink == null) {
+//             alert('Vui lòng thử lại!');
+//             return;
+//         }
+//
+//         var jsSong = {
+//             name: txtName.value,
+//             description: txtDescription.value,
+//             singer: txtSinger.value,
+//             author: txtAuthor.value,
+//             thumbnail: txtThumbnail.value,
+//             link: txtLink.value
+//         }
+//         createSong(jsSong);
+//     }
+// });
+//
+// function createSong(jsSong) {
+//     var xhr = new XMLHttpRequest();
+//     xhr.onreadystatechange = function () {
+//         if (this.readyState == 4 && this.status == 201) {
+//             var song = JSON.parse(this.responseText);
+//             alert(`Lưu thành công bài hát ${song.name}`);
+//         } else if (this.readyState == 4 && (this.status == 401 || this.status == 403)) {
+//             alert('This action required logged in to continue!')
+//             window.location.href = "login.html";
+//         }
+//     }
+//     xhr.open('POST', CREATE_SONG_API, true);
+//     xhr.setRequestHeader("Content-type", "application/json");
+//     xhr.setRequestHeader("Authorization", "Basic " + localStorage.getItem('token-key'));
+//     xhr.send(JSON.stringify(jsSong));
+// }
+//
+//
+//
+var validater = $('#song-form').validate({
+    rules: {
+        name: {
+            required: true,
+            minlength: 1,
+            maxlength: 15
+        },
+        singer: {
+            required: true,
+            minlength: 1,
+            maxlength: 15
+        },
+        description: {
+            required: true,
+            minlength: 1,
+        },
+        author: {
+            required: true,
+            minlength: 1,
+        },
+        thumbnail: {
+            required: true,
+            minlength: 1,
+        },
+        link: {
+            required: true,
+            minlength: 1,
 
-document.addEventListener('DOMContentLoaded', function () {
-    var songForm = document.forms['song-form'];
-    if (songForm == null || songForm['btn-submit'] == null) {
-        alert('Vui lòng thử lại!');
-        return;
-    }
-    songForm['btn-submit'].onclick = function () {
-        var txtName = songForm['name'];
-        var txtDescription = songForm['description'];
-        var txtSinger = songForm['singer'];
-        var txtAuthor = songForm['author'];
-        var txtThumbnail = songForm['thumbnail'];
-        var txtLink = songForm['link'];
-        if (txtName == null
-            || txtDescription == null
-            || txtSinger == null
-            || txtAuthor == null
-            || txtThumbnail == null
-            || txtLink == null) {
-            alert('Vui lòng thử lại!');
-            return;
-        }
+        },
+    },
+    messages: {
+        name: {
+            required: '..',
+            minlength: 'Tên quá ngắn, vui lòng nhập ít nhất {0} ký tự',
+            maxlength: 'Tên quá dài, vui lòng nhập nhiều nhất {0} ký tự',
+        },
+        singer: {
+            required: 'Vui lòng nhập họ của bạn.',
+            minlength: 'Họ quá ngắn, vui lòng nhập ít nhất {0} ký tự',
+            maxlength: 'Họ quá dài, vui lòng nhập nhiều nhất {0} ký tự',
+        },
+        description: {
+            required: '..',
+            minlength: 'Tên quá ngắn, vui lòng nhập ít nhất {0} ký tự',
+        },
+        author: {
+            required: '..',
+            minlength: 'Tên quá ngắn, vui lòng nhập ít nhất {0} ký tự',
+        },
+        thumbnail: {
+            required: '..',
+            minlength: 'Tên quá ngắn, vui lòng nhập ít nhất {0} ký tự',
+        },
 
-        var jsSong = {
-            name: txtName.value,
-            description: txtDescription.value,
-            singer: txtSinger.value,
-            author: txtAuthor.value,
-            thumbnail: txtThumbnail.value,
-            link: txtLink.value
-        }
-        createSong(jsSong);
+        link: {
+            required: '..',
+            minlength: 'Tên quá ngắn, vui lòng nhập ít nhất {0} ký tự',
+        },
+    },
+    submitHandler: function (form, event) {
+        event.preventDefault();
+        var senderObject = {
+            name: $(form['name']).val(),
+            singer: $(form['singer']).val(),
+            description: $(form['description']).val(),
+            author: $(form['author']).val(),
+            thumbnail: $(form['thumbnail']).val(),
+            link: $(form['link']).val(),
+        };
+        $.ajax({
+            url: CREATE_SONG_API,
+            type: 'POST',
+            contentType: 'application/json; charset=utf-8',
+            data: JSON.stringify(senderObject),
+            headers: { 'authorization': "Basic "+localStorage.getItem('my-token') },
+            success: function (data, textStatus, jqXHR) {
+                alert(`Lưu thành công bài hát ${data.name}`);
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                if (Object.keys(jqXHR.responseJSON.error).length > 0) {
+                    $('#summary')
+                        .text(`please fix ${Object.keys(jqXHR.responseJSON.error).length} below!`);
+                    validater.showErrors(jqXHR.responseJSON.error);
+                    console.log(jqXHR.responseJSON.error);
+                    console.log(jqXHR)
+                }
+
+            }
+        });
+        return false;
     }
 });
 
-function createSong(jsSong) {
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 201) {
-            var song = JSON.parse(this.responseText);
-            alert(`Lưu thành công bài hát ${song.name}`);
-        }
-    }
-    xhr.open('POST', CREATE_SONG_API, true);
-    xhr.setRequestHeader("Content-type", "application/json");
-    xhr.setRequestHeader("Authorization", "Basic " + localStorage.getItem('token-key'));
-    xhr.send(JSON.stringify(jsSong));
+function formatDate(date) {
+    var d = new Date(date),
+        month = ' ' + (d.getMonth() + 1),
+        day = d.getDate() < 10 ? '0' + d.getDate() : d.getDate(),
+        year = d.getFullYear();
+    if (month.length < 2) month = '0' + month;
+    if (day.length < 2) day = '0' + day;
+    return [year, month, day].join('-');
 }
-
-
-
